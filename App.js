cf34fc50472939015bdd20a0074854200217ac69
arekm213 <Arqm213@gmail.com>
2023-05-07 21:08:13 +0200

    Audio dont stop playing after pressing done button

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import * as WebBrowser from 'expo-web-browser'; 
 
 export default function App() {
+
+  const openWebBrowser = async() =>{
+    await WebBrowser.openBrowserAsync("https://twitter.com/jonnym1ller/status/1642276906749005824?s=20");
+    // Uncomment below line to get issue fixed
+    // WebBrowser.dismissBrowser();
+  };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <TouchableOpacity style={styles.webBrowserButton} onPress={openWebBrowser}>
+        <Text>Open link</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -17,5 +28,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  webBrowserButton: {
+    marginTop: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    backgroundColor: 'orange',
   },
 });
